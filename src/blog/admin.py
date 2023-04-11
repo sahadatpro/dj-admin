0da3345346752f87bf45ea1ac6ae1d0de3230914
sahadatpro <sahadatsays@gmail.com>
2023-04-12 01:50:02 +0600

    dj admin custom form and  admin form set

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from blog.models import Post, Category
+from django import forms
 
 class BlogAdminArea(admin.AdminSite):
     site_header = "Blog Admin Area"
@@ -12,8 +13,33 @@ blog_admin = BlogAdminArea(name="BlogAdmin")
 
 blog_admin.register(Category)
 
+class PostAdminForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        exclude = ['']
+    
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'status', 'publish', 'created']
+
+    # form = PostAdminForm
+    # exclude = ['']
+
+    fieldsets = (
+        ('Basic Section', {
+        'fields': ('title', 'slug', 'category')
+        }),
+        ('Explaination', {
+        'fields': ('excerpt', 'description'),
+        # 'classes': ('wide', 'extrapretty')
+        }),
+        ('Controlling Section', {
+        'fields': ('author', 'status'),
+        'classes': ('collapse', )
+        })
+    )
 
 blog_admin.empty_value_display = "(None)"
 
